c1397536ee567ab8c36148e9845b010ef3b48305
Yogesh Parmar <yp81832@gmail.com>
2021-08-30 16:07:02 +0530

    New Pages added with New Navbar
    Four pages are remaining - Our Philosophy, Gallery, Admission, News.

---
+++ Jteacher/Dep_COMMERCE.php
@@ -0,0 +1,230 @@
+﻿<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
+<html lang="en">
+<!--<![endif]-->
+<head>
+    <!-- Basic Page Needs
+  ================================================== -->
+    <meta charset="utf-8">
+    <title>Commerce</title>
+    <meta name="description" content="MGM's Sanskar Mahavidhyalay">
+    <meta name="author" content="Mahatma Gandhi Mission Sanskar Vidyalaya">
+    <!-- Mobile Specific Metas
+  ================================================== -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <!-- CSS
+  ================================================== -->
+    <link rel="stylesheet" href="css/zerogrid.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    <link rel="stylesheet" href="css/responsiveslides.css" />
+    <!--[if lt IE 8]>
+       <div style=' clear: both; text-align:center; position: relative;'>
+         <a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
+           <img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." />
+        </a>
+      </div>
+    <![endif]-->
+    <!--[if lt IE 9]>
+		<script src="js/html5.js"></script>
+		<script src="js/css3-mediaqueries.js"></script>
+	<![endif]-->
+    <link href='./images/favicon.ico' rel='icon' type='image/x-icon' />
+    <!-- -----Menu Item-------------->
+    <link href="css/Menu.css" rel="stylesheet" type="text/css" />
+    <!-- -----Menu Item-------------->
+
+    <script src="js/jquery.min.js"></script>
+
+    <script src="js/responsiveslides.js"></script>
+
+    <script src="fancybox/jquery.fancybox-1.3.4.pack.js" type="text/javascript"></script>
+
+    <link href="fancybox/jquery.fancybox-1.3.4.css" rel="stylesheet" type="text/css" />
+
+    <script type="text/javascript">
+	  
+	  $(function () {
+      $("#slider").responsiveSlides({
+        auto: true,
+        pager: false,
+        nav: true,
+        speed: 500,
+        maxwidth: 960,
+        namespace: "centered-btns"
+      });
+    });
+	
+    //	-----------------------------fancybox
+		$(document).ready(function() {
+			
+			$(".popup").fancybox({
+				'width'				: '64%',
+				'height'			: '73%',
+				'autoScale'			: false,
+				'transitionIn'		: 'none',
+				'transitionOut'		: 'none',
+				'type'				: 'iframe'
+			});
+
+			 	$("#Mission").fancybox({
+				'width'				: '50%',
+				'height'			: '40%',
+				'autoScale'			: false,
+				'transitionIn'		: 'none',
+				'transitionOut'		: 'none',
+				'type'				: 'iframe'
+			});
+		 });
+	 //	-----------------------------fancybox
+	 
+				
+	 
+    </script>
+
+	<!-- Calender Start -->
+	
+	<!-- Core CSS File. The CSS code needed to make eventCalendar works -->
+	<link rel="stylesheet" href="css/eventCalendar.css">
+
+	<!-- Theme CSS file: it makes eventCalendar nicer  -->
+	<link rel="stylesheet" href="css/eventCalendar_theme_responsive.css">
+	
+	<!-- Calender End -->
+	
+	
+</head>
+<body>
+ 
+    <script src="js/script.js" type="text/javascript"></script>
+
+    <!--------------Header--------------->
+    <header> 
+	<img  style="position: absolute;top:25px; left:15px;height:100px; width:900px;" src="./images/example1.png"/>
+	
+	 
+</header>
+    <!--------------Navigation--------------->
+    <div class="container">
+        <a class="toggleMenu" href="#" style="display: none;">Menu</a>
+			<?php include ('Menu.php');?>
+    </div>
+    <!--------------Slideshow--------------->
+    <section class="featured">
+	<div class="rslides_container"> 
+		<ul class="rslides" id="slider">
+<li><img src="images/slide1.jpg"/></li>
+			<li><img src="images/slide2.jpg"/></li>
+			<li><img src="images/slide4.jpg"/></li>
+			<li><img src="images/slide5.jpg"/></li>
+			<li><img src="images/slide9.jpg"/></li>
+			<li><img src="images/slide10.jpg"/></li>
+			<li><img src="images/slide11.jpg"/></li>
+			<li><img src="images/slide12.jpg"/></li>
+		 </ul>
+	</div>
+</section>
+    <!--------------Content--------------->
+    <section id="content">
+	<div class="zerogrid">
+		<div class="row">
+			<div class="main-content">
+				 <div class="row">
+					<div class="col-2-3">
+					 
+						<article>
+							<div class="heading">
+								<h2><a href="Dep_COMMERCE.php"><u>Commerce</u></a></h2>
+							</div>
+							<div class="content">
+								<div style="margin:30px">
+									<p>The Junior Wing of the college offers courses in 'Commerce'  under the rules and regulations of the Maharashtra State Board Secondary and Higher Secondary Education.
+									<br>Commerce College which has been steadily and surely growing and developing with outstanding academic results and all other achievements. Understanding the need for management education, the Institute of Management, Education, Research and training was established. 
+									</p>
+
+									<div class="col-1-3">
+										<section>
+										
+										 <div class="content">
+											<a class="popup" href="Jteacher/Commerce Mrs. Kalyani  Chavan.html"> <img class="teacher" src="Jteacher/img/Commerce Mrs. Kalyani  Chavan.JPG" /></a>
+											 <div class="heading">
+												<h1><a class="popup" href="Jteacher/Commerce Mrs. Kalyani  Chavan.html">Mrs. Kalyani  Chavan</a></h1>
+												<h1>M.Com,B.Ed</h1>
+											</div>
+												<p>  </p>
+											</div>
+										</section>
+									</div> 
+									
+									<div class="col-1-3">
+										<section>
+											
+											<div class="content">
+												<a class="popup" href="Jteacher/sheetalnabariya.html"><img class="teacher" src="Jteacher/img/sheetalnabariya.jpg"/></a>
+												
+												<div class="heading">
+												<h1><a class="popup" href="Jteacher/sheetalnabariya.html">Mrs.Sheetal Nabariya</a></h1>
+												<h1>M.B.S.(Finance), M.B.A.(Marketing), M.Com, B.Ed</h1>
+												
+											</div>
+												<p> </p>
+											 </div>
+										</section>
+									</div>
+								 
+								</div>
+								
+							</div>
+						</article>
+						 
+					</div>
+					
+					 
+					
+					
+					
+					 
+					 	
+					<div class="col-2-3">
+					 
+							<!-- <div class="heading">
+								<h2><a href=#>आमच्याबद्दल</a></h2>
+							</div>
+							<div class="content">
+								<p>उपरोल्लेखित ध्येय डोळयासमोर ठेवून सन २००३-०४ मध्ये महाराष्ट्र शासन व शालेय शिक्षण खात्याच्या मान्यतेने पूर्व प्राथमिक व माध्यमिक शाळा सुरु केली. अवघ्या ८५ विदयाथ्र्यांना घेऊन शाळेने आपला प्रवास चालु केला २००३-०४ ते २०११-१२ पर्यंतची शाळेची संख्यात्मक स्थिती पुढील आलेखावरुन सहज लक्षात येईल.
+								<a id="A2" href="Aboutus.php">पुढे</a>
+								
+								</p>
+
+								
+							</div>  -->
+						 
+					</div>
+					
+					
+					
+					 
+					
+				 </div>
+			 </div>
+		</div>
+	</div>
+</section>
+    <!--------------Footer--------------->
+    <footer>
+<div class="zerogrid">
+<?php include ('Footer.php');?>
+</div> 
+</footer>
+
+</body>
+
+<!-- Calender Start -->
+ 
+<script src="js/jquery.eventCalendar.js" type="text/javascript"></script>
+<!-- Calender End -->
+ 
+</html>
